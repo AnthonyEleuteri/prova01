065bcb51473653576dfaf8b0b8f5fae7df587741
AnthonyEleuteri <anthony.eleuteri@tudenti.unicam.it>
2022-10-21 13:56:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+﻿//console.log("Ecco il mio primo javascript!");
+
+//var sonoUnInt = 10;
+
+//console.log("Ecco il mio int:",sonoUnInt="ciao");
+
+//console.log("Ecco il mio vero int:", sonoUnInt=10);
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send('Hello Wooooooorld2!')
+})
+
+app.listen(port,() => {
+    console.log('Example app listening at http://localhost:${port}')
+})
---
+++ prova01/Program.fs
@@ -0,0 +1,9 @@
+﻿(*
+Dati due interi 'a' e 'b' restituisco il loro massimo comun divisore.
+
+a > 0
+b > 0
+*)
+let rec mcd(a,b) =
+    if(b = 0) then a
+    else mcd(b, a%b)
